ca16fccd68a969c1be8991339135526addbfcb47
Renan <renan.p.silva@hotmail.com>
2024-09-04 23:59:52 -0300

    learning how a cup is made and configuring it to meet the standard

--- curso-angular-app/src/app/app.component.ts
+++ curso-angular-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { headerComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [headerComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
